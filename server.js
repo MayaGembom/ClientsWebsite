55c87da43e01d9012eb6a593a34f6eb728241d25
MayaGembom <mayagembom3007@gmail.com>
2022-02-05 14:38:30 +0200

    connect to express

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+
+
 
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
